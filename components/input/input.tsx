bc77d5e83220b498aca9f561018c78f1f887aadc
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-12-15 17:27:51 +0100

    login page is ready

--- components/input/input.tsx
+++ components/input/input.tsx
@@ -1,25 +1,30 @@
 import { InputHTMLAttributes } from "react";
 
-import styles from './input.module.scss';
-import uStyles from '../../styles/utils.module.scss';
+import styles from "./input.module.scss";
+import uStyles from "../../styles/utils.module.scss";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement>{
-  name?: string;
-  label?: string;
-  type: string;
-  addSuffix?: boolean;
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+	name?: string;
+	label?: string;
+	type: string;
+	addSuffix?: boolean;
 }
 
-const Input = ({name, addSuffix = false , type, label, children, ...rest}: InputProps) => {
-    return (
-      <div className={`${styles.inputGroup}`}>
-              <input name={name} type={type} {...rest}/>
-              { addSuffix ? 
-                children
-                : 
-                null }
-      </div>
-    );
+const Input = ({
+	name,
+	addSuffix = false,
+	type,
+	label,
+	children,
+	...rest
+}: InputProps) => {
+	return (
+		<div className={`${styles.inputGroup}`}>
+			<input name={name} type={type} {...rest} />
+			{label ? <label htmlFor={label}> {label} </label> : null}
+			{addSuffix ? children : null}
+		</div>
+	);
 };
 
 export default Input;
--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import cx from "classnames";
 
 import styles from "./index.module.scss";
@@ -6,10 +8,29 @@ import uStyles from "../../styles/utils.module.scss";
 
 import Input from "../../components/input/input";
 import Button from "../../components/button/button";
-import { useState } from "react";
+import { useRouter } from "next/dist/client/router";
 
 const Login = () => {
+	const router = useRouter();
 	const [isLogin, setIsLogin] = useState(true);
+	const [isRegistered, setIsRegistered] = useState(false);
+
+	useEffect(() => {
+		if (router.query.name === "register") {
+			setIsLogin(false);
+		}
+	}, []);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		if (isLogin) {
+			return alert("USER LOGGED IN");
+		}
+
+		setIsRegistered(true);
+		return alert("User registered");
+	};
 
 	return (
 		<>
@@ -17,36 +38,97 @@ const Login = () => {
 				<span className={styles.divider}></span>
 
 				<div className={uStyles.container}>
-					<header className={styles.loginHeader}>
-						<p
-							className={cx({ [styles.isActive]: isLogin })}
-							onClick={() => setIsLogin(true)}
-						>
-							Login
-						</p>
-						<p
-							className={cx({ [styles.isActive]: !isLogin })}
-							onClick={() => setIsLogin(false)}
-						>
-							Register
-						</p>
-						{console.log("isLogin: ", isLogin)}
-					</header>
-
-					<form>
-						<Input type="text" placeholder="Email" />
-						<Input type="password" placeholder="Password" />
-						<Link href="#">
-							<a>
-								<small>Forgot password?</small>
-							</a>
-						</Link>
-						<div className={styles.rememberMe}>
-							<Input type="checkbox" id="remember-me" value="remember-me" />
-							<label htmlFor="remember-me">remember me</label>
+					<div className={styles.flexContainer}>
+						<div className={styles.leftBlock}>
+							<div className={styles.imageWrapper}>
+								<Image
+									src="/img/login.jpg"
+									layout="fill"
+									objectFit="cover"
+									objectPosition='0 67%'
+								></Image>
+							</div>
 						</div>
-						<Button title="Login" type="submit" backgroundColor='dark' />
-					</form>
+						<div className={styles.rightBlock}>
+							<header className={styles.loginHeader}>
+								<p
+									className={isLogin ? styles.isActive : null}
+									onClick={() => {
+										setIsLogin(true);
+										setIsRegistered(false);
+									}}
+								>
+									Login
+								</p>
+								<p
+									className={!isLogin ? styles.isActive : null}
+									onClick={() => setIsLogin(false)}
+								>
+									Register
+								</p>
+							</header>
+							<form onSubmit={handleSubmit}>
+								{isLogin ? (
+									<>
+										<Input type="text" placeholder="Email" />
+										<Input type="password" placeholder="Password" />
+										<div className={styles.furtherActions}>
+											<Link href="/">
+												<a>
+													<small>Forgot password?</small>
+												</a>
+											</Link>
+											<div className={styles.rememberMe}>
+												<Input
+													type="checkbox"
+													label="Remember me"
+													id="Remember me"
+												/>
+											</div>
+										</div>
+									</>
+								) : (
+									<div className={styles.register}>
+										{!isRegistered ? (
+											<>
+												<h3>Still Not Registered?</h3>
+												<p>
+													Register now to start shopping, we just need you
+													e-mail to begin with!
+												</p>
+												<Input type="text" placeholder="Email *" />
+											</>
+										) : (
+											<>
+												<h3>Well Done!</h3>
+												<p>
+													You’re ready to shop with us. Don’t forget to change
+													your password by clicking here: <br />
+													<Link href="/">
+														<a>Change password</a>
+													</Link>
+													<br /> <br /> or
+												</p>
+											</>
+										)}
+									</div>
+								)}
+
+								{!isRegistered ? (
+									<Button
+										title={isLogin ? "Login" : "Register"}
+										type="submit"
+									/>
+								) : (
+									<Link href="/" passHref>
+										<a>
+											<Button title="Start Shopping Now!" />
+										</a>
+									</Link>
+								)}
+							</form>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
