ba6561182453ba3aafd49df3c3f3b8cc36c5242b
Yuri Rech <yuri.nunes@tuntscorp.com>
2020-12-15 18:45:01 +0100

    theming

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,13 +7,13 @@ import Footer from "../components/footer/footer";
 
 function App({ Component, pageProps }: AppProps) {
 	return (
-		<>
+		<div className='autumn-theme'>
 			<MenuBar />
 			<Component {...pageProps} />
 			<footer>
 				<Footer />
 			</footer>
-		</>
+		</div>
 	);
 }
 
